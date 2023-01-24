ab64b4cab6644d302447bc0e792bbfbc0756521e
John Rothfels <john.rothfels@gmail.com>
2023-01-24 10:48:56 -0800

    add SmartInput

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="beforeInteractive" src="https://cdn.tailwindcss.com" />
+      <Component {...pageProps} />
+    </>
+  );
 }
